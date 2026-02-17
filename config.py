d34452871bb08f21e95cc81ea538f8bf89fc4c9d
JJos00 <josejuangh09@gmail.com>
2026-02-16 21:00:07 -0600

    Creacion de la tabla 16/02/2026

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     SECRET_KEY="ClaveSecreta"
     SESSION_COOKIE_SECURE=False
 
-class developmentConfig(Config):
+class DevelopmentConfig(Config):
     DEBUG=True
-    SQLALCHEMY_DATABASE_URI="mysql+pymsql://root:root@127.0.0.1/bdidgs803"
+    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:root@127.0.0.1/bdidgs803"
     SQLALCHEMY_TRACK_MODIFICATIONS=False
