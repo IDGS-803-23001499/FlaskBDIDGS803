dccafeecd31a0b1aa7dd68a9b44233bd1cb242c3
JJos00 <josejuangh09@gmail.com>
2026-02-19 23:24:40 -0600

    Detalles 19/02/2026

--- app.py
+++ app.py
@@ -16,28 +16,27 @@ csrf=CSRFProtect()
 def page_not_found(e):
 	return render_template("404.html"),404
 
-@app.route("/",methods=['GET','POST'])
-@app.route("/index")
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/index")	
 def index():
 	create_form=forms.UserForm(request.form)
 	alumno=Alumnos.query.all()
-	return render_template("index.html",form=create_form,alumno=alumno)
+	return render_template("index.html", form=create_form,alumno=alumno)
 
-@app.route("/Alumnos",methods=['GET','POST'])
+@app.route("/Alumnos", methods=['GET', 'POST'])
 def alumnos():
 	create_form=forms.UserForm(request.form)
-	if request.method=='POST':
+	if request.method == 'POST':
 		alum=Alumnos(nombre=create_form.nombre.data,
-			   		apaterno=create_form.apaterno.data,
-					email=create_form.email.data)
+			   		 apaterno=create_form.apaterno.data,
+				     email=create_form.email.data)
 		db.session.add(alum)
 		db.session.commit()
 		return redirect(url_for('index'))
-	return render_template("Alumnos.html",form=create_form)
+	return render_template("Alumnos.html", form=create_form)
 
-@app.route("/detalles",methods=['GET','POST'])
+@app.route("/detalles", methods=['GET', 'POST'])
 def detalles():
-	create_form=forms.UserForm(request.form)
 	if request.method=='GET':
 		id=request.args.get('id')
 		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
@@ -45,7 +44,7 @@ def detalles():
 		nombre=alum1.nombre
 		apaterno=alum1.apaterno
 		email=alum1.email
-	return render_template("detalles.html",nombre=nombre,apaterno=apaterno,email=email)
+		return render_template("detalles.html",nombre=nombre,apaterno=apaterno,email=email)
 
 if __name__ == '__main__':
 	csrf.init_app(app)
